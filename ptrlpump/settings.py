36e219fa9348460806e38b87dbe32bdb0a29fc1b
sreerangk <sreerangk77@gmail.com>
2022-04-01 14:08:09 +0530

    init

--- ptrlpump/settings.py
+++ ptrlpump/settings.py
@@ -131,5 +131,8 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
